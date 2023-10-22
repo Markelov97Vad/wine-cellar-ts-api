c227a355904fff04d13e82a5728a81b63aeea20f
Vadim Markelov <Vedmakmarkelov@yandex.ru>
2023-10-22 16:45:00 +0300

    поиск ошибки

--- middlewares/auth.ts
+++ middlewares/auth.ts
@@ -5,8 +5,15 @@ import { UNAUTHORIZED_AUTH_MESSAGE, checkJWT } from "../utils/config";
 
 
 const auth = (req: any, res: Response, next: NextFunction) => {
+  // const token = req.cookies.jwt;
+  // console.log('secret',req.secret);
+  // // console.log('cookie',req.rawHeaders[req.rawHeaders.length - 1].slice(4));
+  // console.log('cookie',req.rawHeaders);
+  console.log('cookie',req.cookies);
+
+  // const token = req.rawHeaders[req.rawHeaders.length - 1].slice(4);
   const token = req.cookies.jwt;
-  console.log('token',token);
+  // console.log('token',token);
 
 
   if(!token) {
